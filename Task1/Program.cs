c2bef453a483a5d6ba533e6bdf2baa279996bed9
Ваше имя <garaevaea@mail.ru>
2022-02-25 20:26:46 +0300

    Add Task1, Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberA = new int();
+int numberB = new int();
+
+Console.WriteLine("Введите первое число:");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB) 
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго числа");
+}
+
